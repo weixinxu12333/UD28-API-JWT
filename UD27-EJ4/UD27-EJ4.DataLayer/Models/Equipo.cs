78fa5639f9e7b9f6ba1de937e1c3e29cb9b104b8
Weixin <weixin.xu.2018@gmail.com>
2021-02-15 22:30:43 +0100

    EJ4 ACABADO

---
+++ UD27-EJ4/UD27-EJ4.DataLayer/Models/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD27_EJ4.DataLayer.Models
+{
+    public class Equipo
+    {
+        public string NumSerie { get; set; }
+        public string Nombre { get; set; }
+        public Facultad Facultad { get; set; }
+        public ICollection<Reserva> Reservas { get; set; }
+    }
+}
---
+++ UD27-EJ4/UD27-EJ4/Controllers/ReservasController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UD27_EJ4.DataLayer.Context;
+using UD27_EJ4.DataLayer.Models;
+
+namespace UD27_EJ4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservasController : ControllerBase
+    {
+        private readonly InvestigadoresContext _context;
+
+        public ReservasController(InvestigadoresContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reserva>>> GetReservas()
+        {
+            return await _context.Reservas.ToListAsync();
+        }
+
+        // GET: api/Reservas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reserva>> GetReserva(string id)
+        {
+            var reserva = await _context.Reservas.FindAsync(id);
+
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return reserva;
+        }
+
+        // PUT: api/Reservas/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReserva(string id, Reserva reserva)
+        {
+            if (id != reserva.Investigador_DNI)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reserva).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservas
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Reserva>> PostReserva(Reserva reserva)
+        {
+            _context.Reservas.Add(reserva);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReservaExists(reserva.Investigador_DNI))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetReserva", new { id = reserva.Investigador_DNI }, reserva);
+        }
+
+        // DELETE: api/Reservas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reserva>> DeleteReserva(string id)
+        {
+            var reserva = await _context.Reservas.FindAsync(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservas.Remove(reserva);
+            await _context.SaveChangesAsync();
+
+            return reserva;
+        }
+
+        private bool ReservaExists(string id)
+        {
+            return _context.Reservas.Any(e => e.Investigador_DNI == id);
+        }
+    }
+}
